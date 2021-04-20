c417921ef63a5e3206dd3a1a7e4db63cb8921866
henrymth <henrymth@live.com>
2021-04-20 08:46:11 +0700

    apply refactoring, main method jadi lebih sedikit code nya. dengan cara bikin 2 method baru readnumber dan calculatemortgate

--- src/com/mudah_technology_mortgage_mosh/Main.java
+++ src/com/mudah_technology_mortgage_mosh/Main.java
@@ -5,46 +5,55 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        int principal =0;
-        float monthlyInterest =0;
-        int numberOfPayments =0;
+        int principal = (int) readNumber("Principal / Jumlah pinjaman (1000-1jt) : ", 1000, 500_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 50);
+        byte years = (byte) readNumber("Period (Years): ",1,20);
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Principal / Jumlah pinjaman (1000-1jt) : ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 500_000_000) break;
-            System.out.println("Enter a value between 1000 and 1 000 000");
-        }
-
-        while (true) {
-            System.out.print("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
-            if (annualInterest >= 1 && annualInterest <= 50) break;
-            System.out.println("Enter a value between 5 and 50");
-        }
-
-        while (true) {
-            System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
-            numberOfPayments = years * MONTH_IN_YEAR;
-            if (years > 1 && years <= 20) break;
-            System.out.println("Masuk jumlah tahun yang bener dong!");
-        }
-
-        double mortgage = principal
-                    * (monthlyInterest * Math.pow(1 + monthlyInterest,numberOfPayments)
-                    / (Math.pow(1 + monthlyInterest,numberOfPayments) - 1));
+        double mortgage = calculateMortgage(principal, annualInterest, years);
 
         //String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
+    }
+
+    public static double readNumber (String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max) break;
+            System.out.println("Enter a value between " + min +  " and " + max + "");
+        }
+        return value;
+    }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years){
+
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+        short numberOfPayments = (short)(years * MONTH_IN_YEAR);
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest,numberOfPayments)
+                / (Math.pow(1 + monthlyInterest,numberOfPayments) - 1));
+
+        return mortgage;
+    }
+
+    public static void greetUser(String name){
+        System.out.println(fnGreetUser(name));
+    }
+
+    public static String fnGreetUser (String name) {
+        String tempgreetUser = "Hello " + name;
+        return tempgreetUser;
     }
 }
